90c0cf87400e5ed17dfa46cb8aa8a65d742e9384
zarif007 <zarifhuq786@gmail.com>
2023-05-22 01:28:50 +0600

    Added fetching instructor info from activity state

--- src/services/activityState.service.ts
+++ src/services/activityState.service.ts
@@ -10,7 +10,9 @@ const getActivityStateService = async (query: object) => {
 
 const getActivityStateByActivityIdService = async (id: string) => {
   const filter = { activityId: id };
-  const activityState = await ActivityStateModel.findOne(filter).populate("activityId");
+  const activityState = await ActivityStateModel.findOne(filter)
+    .populate("activityId")
+    .populate("instructor");
   return activityState;
 };
 
@@ -20,29 +22,34 @@ const addActivityStateService = async (activityState: any) => {
   return result;
 };
 
-const updateActivityStateByActivityIdService = async (id: string, updateState: any) => {
-  const result = await ActivityStateModel.findOneAndUpdate({activityId:id}, updateState,{new:true}); 
+const updateActivityStateByActivityIdService = async (
+  id: string,
+  updateState: any
+) => {
+  const result = await ActivityStateModel.findOneAndUpdate(
+    { activityId: id },
+    updateState,
+    { new: true }
+  );
   return result;
 };
 
-
 const bookSeatByActivityStateIdService = async (id: string) => {
-  const updateState = { $inc: { bookedSeat: 1 } }
-  const result = await ActivityStateModel.findByIdAndUpdate(id, updateState,{new:true});
+  const updateState = { $inc: { bookedSeat: 1 } };
+  const result = await ActivityStateModel.findByIdAndUpdate(id, updateState, {
+    new: true,
+  });
   return result;
 };
 
 const checkActivityStateStatusService = async (query: object) => {
   const selection = { activityId: 1, bookedSeat: 1, totalSeat: 1 };
   const result = await ActivityStateModel.find(query)
-  .select(selection)
-  .populate("activityId",{name:1});
-  
+    .select(selection)
+    .populate("activityId", { name: 1 });
+
   return result;
-
 };
-
-
 
 export {
   getActivityStateService,
@@ -51,5 +58,5 @@ export {
   updateActivityStateByActivityIdService,
   bookSeatByActivityStateIdService,
   // unbookSeatActivityStateService,
-  checkActivityStateStatusService
+  checkActivityStateStatusService,
 };
