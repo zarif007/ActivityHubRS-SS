b5f704c6d980b7c69efbd4ac80b87ebb79978c78
Danial Islam Shohardo <danialcodes@gmail.com>
2023-05-24 04:50:40 +0600

    Database connection way change

--- dist/utils/dConnect.js
+++ dist/utils/dConnect.js
@@ -14,13 +14,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDb = exports.connectToDatabase = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const dburi = process.env.DATABASE_URI;
-const dbname = process.env.DATABASE_NAME;
+const dbUri = process.env.DATABASE_URI;
+const dbName = process.env.DATABASE_NAME;
 let _db;
 const connectToDatabase = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose_1.default.connect(`${dburi + dbname}`);
-        console.log(`Successfully connected to Database [${dbname}]`);
+        yield mongoose_1.default.connect(`${dbUri}`, { dbName });
+        console.log(`Successfully connected to Database [${dbName}]`);
     }
     catch (err) {
         console.log(err);
--- src/utils/dConnect.ts
+++ src/utils/dConnect.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
-const dburi:any = process.env.DATABASE_URI;
-const dbname:any = process.env.DATABASE_NAME;
+const dbUri:any = process.env.DATABASE_URI;
+const dbName:any = process.env.DATABASE_NAME;
 
 
 let _db: any;
 
 export const connectToDatabase = async () => {
     try {
-        await mongoose.connect(`${dburi+dbname}`);
-        console.log(`Successfully connected to Database [${dbname}]`);
+        await mongoose.connect(`${dbUri}`,{dbName});
+        console.log(`Successfully connected to Database [${dbName}]`);
     } catch (err) {
         console.log(err);
     }
