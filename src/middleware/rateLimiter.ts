2de38465e17317e6ab0daf1408bbe6f7de3d2e4c
zarif007 <zarifhuq786@gmail.com>
2023-06-06 12:34:54 +0600

    rete lmiter

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 70,
+  max: 1000,
   message: 'You have exceeded the 70 requests in 24 hrs limit!', 
   standardHeaders: true,
   legacyHeaders: false,
