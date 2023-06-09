a7dc38e022964fac29fd9783ea15392bd1c45ae2
zarif007 <zarifhuq786@gmail.com>
2023-06-09 21:57:53 +0600

    Enabled CORS

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,11 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-<<<<<<< HEAD
-  max: 50, // 50 per IP
-=======
   max: 70,
->>>>>>> 517fb8c439e96524b124d7685e53349cdacd4585
   message: 'You have exceeded the 70 requests in 24 hrs limit!', 
   standardHeaders: true,
   legacyHeaders: false,
