e87a6f072a20f54e06e2940fbbff5282bba7de3d
zarif007 <zarifhuq786@gmail.com>
2023-06-19 00:33:51 +0600

    [ENABLED]: cors

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,9 +1,9 @@
-import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 1000,
-  message: 'You have exceeded the 70 requests in 24 hrs limit!', 
+  max: 120,
+  message: "You have exceeded the 70 requests in 24 hrs limit!",
   standardHeaders: true,
   legacyHeaders: false,
 });
